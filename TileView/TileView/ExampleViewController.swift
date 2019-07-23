06ea055d09d4a83daebea6db1ccf01e96fda52a3
sdubois18 <samdubois18@icloud.com>
2019-07-23 17:24:34 -0400

    Created XCode Project and Main Files

---
+++ TileView/TileView/ExampleViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TileView
+//
+//  Created by Sam DuBois on 7/23/19.
+//  Copyright © 2019 SamDuBois. All rights reserved.
+//
+
+import UIKit
+
+class ExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TileView/TileView/TileView.swift
@@ -0,0 +1,9 @@
+//
+//  TileView.swift
+//  TileView
+//
+//  Created by Sam DuBois on 7/23/19.
+//  Copyright © 2019 SamDuBois. All rights reserved.
+//
+
+import Foundation
